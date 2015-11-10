0359f7c2e7c74e54b4a038a3849cab12d5c2ca20
rahuljc <rahul.jayc@gmail.com>
2015-11-10 23:13:30 +0530

    Trying out javascript after Udemy Course.

---
+++ app.js
@@ -0,0 +1,29 @@
+/**
+* A Javascript music player app.
+*
+*/
+
+(function(context){
+
+	var currentSong = {};
+	var nextSong = {};
+	var prevSong = {};
+
+
+	var defaultSong = {	title: "Uyire - Uyire",	artist: "A.R Rahman",	duration: 60 };
+	var playList = [
+		{title: "Uyire - Uyire",	artist: "A.R Rahman",	duration: 60 },
+		{title: "Newyork Nagaram",	artist: "A.R Rahman",	duration: 50 },
+	];
+
+	function playSong(song){
+		this.currentSong = song || this.defaultSong;
+		console.log(currentSong.title + ' is playing.');
+		setTimeout(function(){
+			console.log("Next song is ");
+		},currentSong.duration*1000)
+	}
+
+	
+})(window)
+
